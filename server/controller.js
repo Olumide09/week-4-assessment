6e96d92020198420d54890c87d6da3cc22818020
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-10-06 10:46:15 -0500

    added the fortune button feature

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,17 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+
+    getFortune: (req, res) => {
+        const fortunes = ["A fresh start would put you on your way", "A friend is a present you give yourself", "A good time to finish up old tasks", "A plesant surprise is waiting for you", "All your hardwork will soon pay off"];
+
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune)
     }
 
 }
